ca6e6cc44ba61ebc122f3b5f27df978045cd9de6
Moraesssa <residencialmoraes507@gmail.com>
2025-09-19 20:55:41 -0300

    Add AppShell layout with responsive alerts drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import AppShell from './app/AppShell';
 
 function App() {
   return (
-    <main className="app">
-      <h1>Prop-Stream</h1>
-      <p>Seu cockpit inteligente para investimentos imobiliários.</p>
-    </main>
+    <AppShell>
+      <section className="app">
+        <h1>Prop-Stream</h1>
+        <p>Seu cockpit inteligente para investimentos imobiliários.</p>
+      </section>
+    </AppShell>
   );
 }
 
